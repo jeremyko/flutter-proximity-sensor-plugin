74332f2d9f525911f28974cdef0cedaae6fa6935
jeremyko <jeremyko69@gmail.com>
2021-03-29 01:38:02 +0900

    remove logging

--- lib/proximity_sensor.dart
+++ lib/proximity_sensor.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
-import 'dart:developer' as developer;
 
 ////////////////////////////////////////////////////////////////////////////////
 class ProximitySensor {
   static EventChannel _streamChannel = EventChannel('proximity_sensor');
   static Stream<int> get events {
     return _streamChannel.receiveBroadcastStream().map((event) {
-      developer.log("sensor value -> " + event.toString());
       return event;
     });
   }
